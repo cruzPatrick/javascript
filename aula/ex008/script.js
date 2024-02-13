11579378caf7f32d4b4fd30052d39f35f6156a07
Patrick Cruz <145387737+cruzPatrick@users.noreply.github.com>
2024-02-12 23:00:21 -0300

    exercicios

---
+++ aula/ex008/script.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+
+console.log(`A velocidade ideal é ${vel} Km/h`)
+if (vel>60){ //Condição Simples
+    console.log('Você passou o limite de velocidade, Multado')
+}
+console.log(`Dirija sem usando cinto de segurança`)
